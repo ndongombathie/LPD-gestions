d4c30674b305bd1a8e8259f3aa2945df69ff8cf2
ndongombathie <ndongo.mbath@univ-thies.sn>
2025-10-30 22:13:12 +0000

    gestion des erreurs

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LoginPage } from './authentification/login/LoginPage'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <LoginPage darkMode={false}/>
+      <h1>Bonjour</h1>
     </>
   )
 }
