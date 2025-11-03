29c245460558acf7f5ca54337a5eca7f02e35930
ndongombathie <ndongo.mbath@univ-thies.sn>
2025-11-03 19:42:25 +0000

    ajouter de l'intercepteur pour ajouter les token

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -10,3 +10,24 @@ export const instance = axios.create(
 
     }
 )
+
+instance.interceptors.request.use((config)=>{
+    const token=localStorage.getItem('token');
+    if(token)
+    {
+        config.headers.Authorization=`Bearer ${token}`;
+    }
+    return config;
+});
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      console.warn("Session expirée. Veuillez vous reconnecter.");
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
