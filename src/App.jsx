b320521618cb2990118d43ee8d5a99ed73de9a77
Aliou Dramé <aliou.drame@univ-thies.sn>
2025-12-25 10:44:28 +0000

    versions 2 du gestionnaire de boutique

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
 
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import LayoutGestionnaire from "./gestionnaire-boutique/components/LayoutGestionnaire";
-
-// Pages du gestionnaire
-import Dashboard from "./gestionnaire-boutique/pages/Dashboard";
-import Produits from "./gestionnaire-boutique/pages/Produits";
-import Stock from "./gestionnaire-boutique/pages/Stock";
-import Historique from "./gestionnaire-boutique/pages/Historique";
-import Alertes from "./gestionnaire-boutique/pages/Alertes";
-import Rapports from "./gestionnaire-boutique/pages/Rapports";
 import AppGestionnaireBoutique from "./gestionnaire-boutique/AppGestionnaireBoutique";
 
 export default function App() {
