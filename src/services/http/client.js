980b8461826f74cb3ed6bd7f1a33ed6d0dcae942
ndongombathie <ndongo.mbath@univ-thies.sn>
2026-04-12 16:00:41 +0000

    reglage les erreurs sur le vendeurs

--- src/services/http/client.js
+++ src/services/http/client.js
@@ -12,7 +12,7 @@
 import axios from 'axios';
 
 // ===== CONFIGURATION =====
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://lpdmanager.com/api';
 
 /**
  * Instance Axios UNIQUE - Ne pas créer d'autres instances!
